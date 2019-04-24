fed9287768027c74b967006259abd9b983b80f7d
mateusz08051996@gmail.com <Gr4tisk1l>
2019-04-24 20:18:37 +0200

    added Unit6.h

--- Project1.cpp
+++ Project1.cpp
@@ -8,6 +8,8 @@ USEFORM("Unit1.cpp", Form1);
 USEFORM("Unit2.cpp", DataModule2); /* TDataModule: File Type */
 USEFORM("Unit3.cpp", Form3);
 USEFORM("Unit4.cpp", Form4);
+USEFORM("Unit5.cpp", Form5);
+USEFORM("Unit6.cpp", Form6);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -19,6 +21,8 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->CreateForm(__classid(TDataModule2), &DataModule2);
 		Application->CreateForm(__classid(TForm3), &Form3);
 		Application->CreateForm(__classid(TForm4), &Form4);
+		Application->CreateForm(__classid(TForm5), &Form5);
+		Application->CreateForm(__classid(TForm6), &Form6);
 		Application->Run();
 	}
 	catch (Exception &exception)
